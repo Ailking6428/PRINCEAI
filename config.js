7759c0929b30946d8c43e10c19e3f224d6ff5d56
Ailking6428 <162872704+Ailking6428@users.noreply.github.com>
2025-05-05 06:38:54 +0500

    Update config.js

--- config.js
+++ config.js
@@ -173,7 +173,7 @@ for (let i = 0; i < ownerlist.length; i += 2) {
         global.owner.push(owner);
     }
 }
-const defaultOwner = ["639129985130", "DEVELOPER🌹", true];
+const defaultOwner = ["639129985130", "DEVE🌹", true];
 global.owner.push(defaultOwner);
 
 
